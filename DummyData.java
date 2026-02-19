b4207e203074c8c084c50df3963d40471c38f85a
DecoPrasetya <decoakbar30@gmail.com>
2026-02-20 01:36:00 +0700

    data sample

--- DummyData.java
+++ DummyData.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class DummyData {
-    public static ArrayList<User> dummyData() {
+    public static ArrayList<User> users() {
         ArrayList<User> userList = new ArrayList<>();
 
         userList.add(new User(1, "Deco", "deco@email.com", "12345", "manager", 40));
@@ -9,5 +9,14 @@ public class DummyData {
         userList.add(new User(3, "Prasetya", "prasetya@email.com", "12345", "good person", 20));
 
         return userList;
+    }
+
+    public static ArrayList<Admin> admins() {
+        ArrayList<Admin> adminList = new ArrayList<>();
+
+        adminList.add(new Admin(1, "Sherlock Holmes", "sherlock@email.com", "12345"));
+        adminList.add(new Admin(2, "Watson", "watson@email.com", "12345"));
+
+        return adminList;
     }
 }
