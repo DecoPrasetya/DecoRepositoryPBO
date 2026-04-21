24e190faf3bcd891e2d620465282a15d818cd057
DecoPrasetya <decoakbar30@gmail.com>
2026-04-22 00:01:11 +0700

    interface pemb e wallet

--- assessment1/PembEWallet.java
+++ assessment1/PembEWallet.java
@@ -1,13 +1,32 @@
-public class PembEWallet extends Pembayaran {
+public class PembEWallet implements Pembayaran {
+    private int id;
+    private String nama;
+    private String nominal;
     private String eWallet;
 
     public PembEWallet(int id, String nama, String nominal, String eWallet) {
-        super(id, nama, nominal);
+        this.id = id;
+        this.nama = nama;
+        this.nominal = nominal;
         this.eWallet = eWallet;
     }
-    
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getNama() {
+        return nama;
+    }
+
+    @Override
+    public String getNominal() {
+        return nominal;
+    }
+
     public String getEWallet() {
         return eWallet;
-
     }
 }
