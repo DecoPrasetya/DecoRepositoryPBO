308fe2f30a56556c21514df1f6e21eefb8ed4635
DecoPrasetya <decoakbar30@gmail.com>
2026-04-21 23:58:07 +0700

    interface pemb bank

--- assessment1/PembBank.java
+++ assessment1/PembBank.java
@@ -1,9 +1,29 @@
-public class PembBank extends Pembayaran {
+public class PembBank implements Pembayaran {
+    private int id;
+    private String nama;
+    private String nominal;
     private String bank;
 
     public PembBank(int id, String nama, String nominal, String bank) {
-        super(id, nama, nominal);
+        this.id = id;
+        this.nama = nama;
+        this.nominal = nominal;
         this.bank = bank;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getNama() {
+        return nama;
+    }
+
+    @Override
+    public String getNominal() {
+        return nominal;
     }
 
     public String getBank() {
