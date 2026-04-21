07e6bc5e7a90551f4e1dd06bc49309b553ccaa65
DecoPrasetya <decoakbar30@gmail.com>
2026-04-21 23:57:54 +0700

    interface pembayaran

--- assessment1/Pembayaran.java
+++ assessment1/Pembayaran.java
@@ -1,23 +1,5 @@
-public class Pembayaran {
-    protected int id;
-    protected String nama;
-    protected String nominal;
-
-    public Pembayaran(int id, String nama, String nominal) {
-        this.id = id;
-        this.nama = nama;
-        this.nominal = nominal;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public String getNominal() {
-        return nominal;
-    }
+public interface Pembayaran {
+    int getId();
+    String getNama();
+    String getNominal();
 }
